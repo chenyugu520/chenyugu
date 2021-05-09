d42b2d7b0bd40fb2b5be262302cf53a9a7acf01a
mhmao <308865616@qq.com>
2021-05-09 11:01:42 +0800

    添加忽略文件

--- Output/segger_rtt.d
+++
@@ -1,5 +0,0 @@
-..\output\segger_rtt.o: ..\TraceRecorder\streamports\Jlink_RTT\SEGGER_RTT.c
-..\output\segger_rtt.o: ..\TraceRecorder\streamports\Jlink_RTT\include\SEGGER_RTT.h
-..\output\segger_rtt.o: ..\TraceRecorder\streamports\Jlink_RTT\include\SEGGER_RTT_Conf.h
-..\output\segger_rtt.o: e:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-..\output\segger_rtt.o: e:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
